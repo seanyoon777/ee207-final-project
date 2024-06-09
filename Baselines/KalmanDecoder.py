4f82775a2b5d50c051d94b4d085536f046d82428
Sean Yoon <76602593+seanyoon777@users.noreply.github.com>
2024-06-09 16:25:49 -0700

    modified dataloader

--- Baselines/KalmanDecoder.py
+++ Baselines/KalmanDecoder.py
@@ -21,7 +21,7 @@ class KalmanDecoder:
             C : noise matrix, defaults to 1. 
         """
         self.C = C 
-        self.pred_dims = 2
+        self.pred_dims = pred_dims
         self.model = None 
     
     @staticmethod
@@ -37,26 +37,30 @@ class KalmanDecoder:
             C (_type_): _description_
         """
         # converts to contiguous arrays and jit-compatible formats for energy efficiency
-        X = np.ascontiguousarray(X, dtype='float32')
-        Z = np.ascontiguousarray(Z, dtype='float32')
+        X = np.ascontiguousarray(X.astype('float32'))
+        Z = np.ascontiguousarray(Z.astype('float32'))
         
         # calculate the transition matrix from X_t to X_{t+1}
         X1 = X[:, :-1]
         X2 = X[:, 1:]
-        A = np.linalg.lstsq(X1 @ X1.T, X2 @ X1.T, rcond=None)[0].T 
+        A = lstsq_optimized(X1 @ X1.T, X1 @ X2.T).T 
+        print("Transition matrix calculated")
         
         # calculate covariance of transition matrix
         residual = X2 - A @ X1
         W = residual @ residual.T / X1.shape[1] / C
+        print("Covariance matrix calculated")
 
         # calculates the measurement matrix from X_t to X_{t+1}
         # This linearly relates hand state to neural firing
-        H = np.linalg.lstsq(X @ X.T, Z @ X.T, rcond=None)[0].T
-        
-        # calculate the covariance of the measurement matrix
-        residual = X - H @ X
-        Q = residual @ residual.T / X1.shape[1]
+        H = lstsq_optimized(X @ X.T, X @ Z.T).T
+        print("Measurement matrix calculated")
 
+        # calculate the covariance of the measurement matrix
+        residual = Z - H @ X
+        Q = residual @ residual.T / X.shape[1]
+        print("Measurement of covariance matrix calculated")
+        
         return A, W, H, Q 
     
     
@@ -72,54 +76,63 @@ class KalmanDecoder:
             _type_: _description_
         """
         # transpose and make data contiguous 
-        X = np.ascontiguousarray(y_train.T, dtype='float32')
-        Z = np.ascontiguousarray(X_train.T, dtype='float32')
+        X = np.ascontiguousarray(y_train.T.astype('float32'))
+        Z = np.ascontiguousarray(X_train.T.astype('float32'))
         
         # compute matrices
         A, W, H, Q = self.update(X, Z, self.C)
         self.model = [A, W, H, Q] 
 
 
-    def predict(self, X_test): 
+    def predict(self, X_test, y_init): 
         A, W, H, Q = self.model 
-        y_test = self._predict(X_test, self.pred_dims, A, W, H, Q)
+        y_test = self._predict(X_test, y_init, self.pred_dims, A, W, H, Q)
         return y_test 
     
 
-    @staticmethod
-    @nb.jit(nopython=True)
-    def _predict(X_test, n_states, A, W, H, Q): 
+    # @staticmethod
+    # @nb.jit(nopython=True)
+    def _predict(self, X_test, y_init, n_states, A, W, H, Q): 
         # transpose and make data contiguous 
-        Z = np.ascontiguousarray(X_test.T, dtype='float32')
-        
+        Z = np.ascontiguousarray(X_test.astype('float32'))
+
         # initialize 
         n_samples = Z.shape[1]
-        states = np.zeros((n_states, n_samples), dtype='float32')  # state predictions
-        P_m    = np.zeros((n_states, n_states), dtype='float32')   # prior error covariance
-        P      = np.zeros((n_states, n_states), dtype='float32')   # posterior error covariance
-        state  = np.zeros((n_states, 1), dtype='float32')          # initial state as zeros
-        
+        states = np.zeros(shape=(n_states, n_samples), dtype='float32')  # state predictions
+        P_m    = np.zeros(shape=(n_states, n_states), dtype='float32')   # prior error covariance
+        P      = np.zeros(shape=(n_states, n_states), dtype='float32')   # posterior error covariance
+        if y_init is None: 
+            state  = np.ones(shape=(n_states, 1), dtype='float32')          # initial state as zeros
+        else: 
+            state  = y_init
+
         # make them contiguous 
-        states = np.ascontiguousarray(states)
-        P_m    = np.ascontiguousarray(P_m)
-        P      = np.ascontiguousarray(P) 
-        state  = np.ascontiguousarray(state)
-        
+        states = np.ascontiguousarray(states.astype('float32'))
+        P_m    = np.ascontiguousarray(P_m.astype('float32'))
+        P      = np.ascontiguousarray(P.astype('float32'))
+        state  = np.ascontiguousarray(state.astype('float32'))
+
         # kalman filter loop 
-        for t in range(1, n_samples): 
+        for t in tqdm(range(1, n_samples)): 
             # predict state and covariance 
             state_m = A @ state 
             P_m = A @ P @ A.T + W 
             
             # calculate system uncertainty and kalman gain
             S = H @ P_m @ H.T + Q 
-            K = P_m @ H.T @ np.linalg.inv(S)
+            K = P_m @ H.T @ np.linalg.pinv(S)
 
             # update and store the updated state
-            state = state_m + K @ (Z[:, t:t+1] - H @ state_m)
+            state = state_m + K @ (Z[:, t] - H @ state_m)
             P     = (np.eye(n_states) - K @ H) @ P_m 
             
             states[:, t] = state[:, 0]
             
         return states.T
 
+@nb.jit(nopython=True) 
+def lstsq_optimized(X: np.ndarray, y: np.ndarray):
+    """Optimized jit-compatible helper function for calculating the Moore-Penrose pseudoinverse, 
+    given by A+ = (AT A)-1 AT. Used for LRR calculation"""
+    W = np.linalg.solve(X.T @ X, X.T @ y) 
+    return W
--- SNNModel/dataloader.py
+++ SNNModel/dataloader.py
@@ -7,81 +7,144 @@ from neurobench.datasets import PrimateReaching
 data_path = './data/indy_20160630_01.mat'
 
 
-class Dataloader(object):
+class Dataloader:
+    def __init__(self, spike_units='all', normalize=True, bin_size=1, bad_ch_cutoff=1):
+        """_summary_
 
-    def __init__(self):
-
-        pr_dataset = PrimateReaching(file_path='data',
-                                     filename="indy_20160630_01.mat",
-                                     num_steps=1,
-                                     train_ratio=0.5,
-                                     bin_width=0.004,
-                                     biological_delay=0,
-                                     download=False)
-
+        Args:
+            spike_units: string of list of spike unit numbers to use (e.g. [0, 2, 4])
+            normalize (bool, optional): _description_. Defaults to True.
+            bin_size (int, optional): _description_. Defaults to 1.
+            ex_cutoff : excludes spiking units that spike less than this cutoff value 
+        """
+        self.pr_dataset = PrimateReaching(file_path='data',
+                                          filename="indy_20160630_01.mat",
+                                          num_steps=1,
+                                          train_ratio=0.5,
+                                          bin_width=0.004,
+                                          biological_delay=0,
+                                          download=False)
         print("Loaded pr dataset")
-
-
-# TODO: are spikes available for different samples?
+        
+        self.spike_units   = spike_units 
+        self.normalize     = normalize 
+        self.bin_size      = bin_size
+        self.bad_ch_cutoff = bad_ch_cutoff
+        
         with h5py.File(data_path, 'r') as data:
-            # Extract the data
-            timesteps = torch.tensor(np.array(data['t'])).flatten()  # Ensure t is a 1D array
-            finger_pos = torch.tensor(np.array(data['finger_pos']).T)  # Ensure proper shape and convert to PyTorch tensor
+            # initialize spike units to use 
+            if spike_units == 'all': 
+                n_sp = data['spikes'].shape[0]
+                self.spike_units = [i for i in range(n_sp)]
+            
+            # get spike matrix and split into train / test set 
+            spike_matrix = self._get_spike_mat(data)
+            trainX, testX, train_finger_pos, test_finger_pos, train_cursor_pos, test_cursor_pos = self._train_test_split(data, spike_matrix)
+            
+            # bin data
+            if self.bin_size > 1: 
+                trainX, train_finger_pos, train_cursor_pos = self._bin_data(trainX, train_finger_pos, train_cursor_pos) 
+                testX, test_finger_pos, test_cursor_pos    = self._bin_data(testX, test_finger_pos, test_cursor_pos)  
+                print(f'Binned into {4*self.bin_size}ms intervals')
+            
+            # convert positions to velocity 
+            trainY, trainY_cursor = self._pos2vel(train_finger_pos, train_cursor_pos)
+            testY, testY_cursor   = self._pos2vel(test_finger_pos, test_cursor_pos)
+            
+            # normalize velocities if necessary 
+            if self.normalize: 
+                trainY, testY = self._normalize(trainY, testY)
+            
+            # exclude channels that are too sparse 
+            trainX, testX = self._exclude_channels(trainX, testX)
+            
+            self.data = trainX, testX, trainY, testY, trainY_cursor, testY_cursor
+            
 
-            # Determine the number of samples (n) and units (k)
-            n = len(timesteps)
-            k = len(data['spikes'][0])
-
-            # Initialize the nxk array with zeros
-            spike_matrix = np.zeros((n, k), dtype=np.float32)
-
-            # Populate the spike_matrix while reading the spike data
-            for unit_idx, channel_ref in enumerate(data['spikes'][0]):
+    def _get_spike_mat(self, data): 
+        # extract timesteps and spike channel params
+        timesteps = torch.tensor(np.array(data['t'])).flatten()  # Ensure t is a 1D array
+        n_ch = len(data['spikes'][0])  # number of channels 
+        n_sp = len(self.spike_units)   # number of spike units to use for each channel
+        
+        # initialize spike matrix with zeros 
+        n = len(timesteps)
+        k = n_sp * n_ch 
+        spike_matrix = np.zeros((n, k), dtype=np.float32)
+        
+        # read spike data and populate spike matrix 
+        for sp_unit in self.spike_units: 
+            for unit_idx, channel_ref in enumerate(data['spikes'][sp_unit]): 
                 channel = data[channel_ref]  # Dereference the channel
-                for unit_ref in channel:
-                    unit_spike_times = np.array(
-                        unit_ref).flatten()  # Dereference the unit spike times and flatten
+                for unit_ref in channel: 
+                    unit_spike_times = np.array(unit_ref).flatten()  # Dereference the unit spike times and flatten
+                    spike_indices    = np.searchsorted(timesteps, unit_spike_times)
+                    spike_indices    = spike_indices[spike_indices < n]  # Ensure indices are within bounds
+                    spike_matrix[spike_indices, sp_unit*n_ch+unit_idx] = 1
+                    
+        return spike_matrix
+    
+    
+    def _train_test_split(self, data, spike_matrix):
+        finger_pos = torch.tensor(np.array(data['finger_pos']).T)  # Ensure proper shape and convert to PyTorch tensor
+        cursor_pos = torch.tensor(np.array(data['cursor_pos']).T)  # Ensure proper shape and convert to PyTorch tensor
+        # get train and test indices
+        train_idx = self.pr_dataset.ind_train
+        test_idx  = self.pr_dataset.ind_test
+        
+        train_neural = spike_matrix[train_idx]
+        test_neural  = spike_matrix[test_idx]
+        
+        train_finger_pos = finger_pos[train_idx][:,:3]  # extract z, -x, -y positions and drop spherical coords
+        test_finger_pos  = finger_pos[test_idx][:,:3]   # extract z, -x, -y positions and drop spherical coords
+        
+        train_cursor_pos = cursor_pos[train_idx]
+        test_cursor_pos  = cursor_pos[test_idx]
+        
+        return train_neural, test_neural, train_finger_pos, test_finger_pos, train_cursor_pos, test_cursor_pos
+        
+        
+    def _bin_data(self, neural, finger_pos, cursor_pos): 
+        n_samples, n_spikes = neural.shape
+        n_bins = int(np.ceil(n_samples / self.bin_size))
+        
+        X = np.zeros(shape=(n_bins, n_spikes), dtype=np.float32)
+        for bin, i in enumerate(range(0, n_samples, self.bin_size)): 
+            X[bin] = np.mean(neural[i:i+self.bin_size], axis=0)
+        
+        # select corresponding y velocities 
+        y_finger_pos = finger_pos[::self.bin_size]
+        y_cursor_pos = cursor_pos[::self.bin_size]
+        
+        return X, y_finger_pos, y_cursor_pos 
 
-                    spike_indices = np.searchsorted(timesteps, unit_spike_times)
-                    spike_indices = spike_indices[spike_indices < n]  # Ensure indices are within bounds
-                    spike_matrix[spike_indices, unit_idx] = 1
 
-        train_kin_data = finger_pos[pr_dataset.ind_train]
-        test_kin_data = finger_pos[pr_dataset.ind_test]
-
-        train_neural_data = spike_matrix[pr_dataset.ind_train]
-        test_neural_data = spike_matrix[pr_dataset.ind_test]
-
-
-        # Load and normalize the data
-        self.trainY = np.transpose(train_kin_data.numpy())
-        t_mean = np.mean(self.trainY, axis=1, keepdims=True)
-        t_std = np.std(self.trainY, axis=1, keepdims=True)
-
-        self.trainY = np.transpose((self.trainY - t_mean) / t_std)
-        ones_column = np.ones((self.trainY.shape[0], 1))
-        self.trainY = np.concatenate((self.trainY, ones_column), axis=1)
-
-        self.testY = np.transpose(test_kin_data.numpy())
-        self.testY = np.transpose((self.testY - t_mean) / t_std)
-
-        ones_column = np.ones((self.testY.shape[0], 1))
-        self.testY = np.concatenate((self.testY, ones_column), axis=1)
-
-        self.trainX = train_neural_data
-        self.testX = test_neural_data
-
-    def getTrainData(self, type):
-        if type == 'KinData':
-            return self.trainY
-        elif type == 'NeuralData':
-            return self.trainX
-
-    def getTestData(self, type):
-        if type == 'KinData':
-            return self.testY
-        elif type == 'NeuralData':
-            return self.testX
-
-    def getData(self):
-        return self.trainX.T, self.trainY.T, self.testX.T, self.testY.T
+    def _pos2vel(self, y_finger_pos, y_cursor_pos): 
+        y_finger_vel = torch.gradient(y_finger_pos, dim=1)[0].numpy()
+        y_cursor_vel = torch.gradient(y_cursor_pos, dim=1)[0].numpy()
+        return y_finger_vel, y_cursor_vel 
+    
+    
+    def _normalize(self, trainY, testY): 
+        trainY_T = np.transpose(trainY)
+        testY_T = np.transpose(testY)
+        
+        mean = np.mean(trainY_T, axis=1, keepdims=True)
+        std  = np.std(trainY_T, axis=1, keepdims=True)
+        
+        trainY_norm = np.transpose((trainY_T - mean) / std)
+        testY_norm  = np.transpose((testY_T - mean) / std)
+        
+        return trainY_norm, testY_norm
+            
+    
+    def _exclude_channels(self, trainX, testX): 
+        train_chs = np.sum(trainX, axis=0) > self.bad_ch_cutoff
+        test_chs  = np.sum(testX, axis=0) > self.bad_ch_cutoff
+        all_chs   = np.logical_and(train_chs, test_chs)
+        
+        return trainX[:,all_chs], testX[:,all_chs]
+    
+    
+    def __call__(self): 
+        return self.data
